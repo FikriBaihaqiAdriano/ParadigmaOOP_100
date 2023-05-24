23eb26e64a343fecb788201cbbbf825c74d8c73c
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2023-05-24 19:12:45 +0700

    recap class anak

--- MainIA/anak.h
+++ MainIA/anak.h
@@ -1,1 +1,14 @@
-#pragma once
+#ifndef ANAK_H
+#define ANAK_H
+
+class anak {
+public:
+	string nama;
+	anak(pNama) :nama(pNama) {
+		cout << "Anak \"" << nama << "\" ada\n";
+	}
+	~anak() {
+		cout << "Anak \"" << nama << "\" tidak ada\n";
+	}
+};
+#endif
